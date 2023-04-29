495e1b0e35e16780d1dfefdd76b94087e287587c
Богдана Бурковська <128155212+bogdanaburkovska@users.noreply.github.com>
2023-04-29 13:59:50 +0300

    2lab

--- 2/.ccls-cache/@home@runner@lab2zavd2pereroblene/main.c
+++
@@ -1,9 +0,0 @@
- #include <stdio.h> //введення бібліотек
-#include <locale.h>
-#include <math.h>
- int main() {
-    float a=7 ,b=12,c=-21;
-    float y=((a-b)/(2*b+c))+((1)/(a));//введення змінних
-    printf("Обчислення=%.2f \n", y); //виведення обчислення з формули
-   return 0;
-     }
